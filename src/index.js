2e1fb12505b4eacad30295bbb34e1001432dbf11
Sholto <madein.mania@gmail.com>
2021-03-10 10:18:45 +0000

    active tab function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import {HeaderGen} from "./header.js";
-import {TabGen} from "./tab.js"
-import {InfoGen} from "./info.js";
-import {MenuGen} from "./menu.js";
+import { HeaderGen } from "./header.js";
+import { TabGen } from "./tab.js"
+import { InfoGen } from "./info.js";
+import { MenuGen } from "./menu.js";
+import { ContactGen } from "./contact.js";
 
 HeaderGen();
 TabGen();
@@ -9,22 +10,39 @@ InfoGen();
 
 const tabs = document.querySelectorAll(".tab")
 const main = document.querySelector(".main")
-console.log(main)
 
 tabs.forEach(tab => {
-  tab.onclick = (e) => {    
+  tab.onclick = (e) => {
+
     let toRemove = document.querySelector(".tab-content");
     let toAdd = e.target.outerText;
+
     if (toRemove) main.removeChild(toRemove);
-    
-    
-    switch(toAdd) {
-        case "Menu":
-            MenuGen();
-            break;
-        case "Info":
-            InfoGen();
-            break;
+
+
+    switch (toAdd) {
+      case "Menu":
+        MenuGen();
+        activeTab(e, "menu");
+        break;
+      case "Info":
+        InfoGen();
+        activeTab(e, "info");
+        break;
+      case "Contact":
+        ContactGen();
+        activeTab(e, "contact");
+        break;
     }
-  }  
+  }
 });
+
+function activeTab(e, tab) {
+
+  let removeActive = document.querySelector(".tab-selected")
+  removeActive.classList.remove("tab-selected")
+
+  if (e.target.classList.contains(`tab-${tab}`)) {
+    e.target.classList.add("tab-selected")
+  }
+}
